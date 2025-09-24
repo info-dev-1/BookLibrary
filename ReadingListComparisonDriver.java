c3465758c51b23208615f3fcfc7435778e016620
info-dev-1 <37985502+info-dev-1@users.noreply.github.com>
2025-09-24 12:06:10 -0500

    Progress on processor class and handling peoples' book reading lists

--- ReadingListComparisonDriver.java
+++ ReadingListComparisonDriver.java
@@ -13,9 +13,33 @@ public class ReadingListComparisonDriver {
      * Note: data from these will also be kept in the Processor class as array instance fields.
     */
     
-    private ArrayList<Book> readingListPerson1;  // The reading list for person 1.
+    private Book[] bookReadingListPerson1;  // The reading list for person 1.
+    
+    private Book[] bookReadingListPerson2;  // The reading list for person 2.
+
+    private ReadingListComparisonProcessor processor;
+
+    private ReadingListComparisonUserInterface userInterface;
+
+
+    public ReadingListComparisonDriver(Book[] bookReadingListPerson1, Book[] bookReadingListPerson2) {
+        this.processor = new ReadingListComparisonProcessor(bookReadingListPerson1, bookReadingListPerson2);
+        this.userInterface = new ReadingListComparisonUserInterface();
+    }
+
+    // could be renamed to something like, runComparison.
+    // TODO: if I get the book reading lists from the user, then I'll probably have to use an ArrayList (somwhere*) b/c we don't know how many books they'll add. *Maybe in the UserInterface class, so I don't have to "repeat myself" by passing ArrayLists to two class constructors. 
+    public static void main(String[] args) {
+
+        Book[] bookReadingListPerson1 = getUserInterface().getPerson1BookReadingListFromUser();
+        Book[] bookReadingListPerson2 = getUserInterface().getPerson2BookReadingListFromUser();
+        ReadingListComparisonDriver driver = new ReadingListComparisonDriver(bookReadingListPerson1, bookReadingListPerson2);
+
+        driver.getProcessor().logComparisonBookReadingLists();  // TODO: decide whether to change this to logBooksInCommon() (doing 1 thing), or keep it (doing 2+ things).
+        driver.getProcessor().computeSharedInterestStatistics();
+        
+        driver.getUserInterface().displaySharedInterestResults();
+    }
     
 
-    private ArrayList<Book> readingListPerson2;  // The reading list for person 2.
-    
-}
+
--- ReadingListComparisonProcessor.java
+++ ReadingListComparisonProcessor.java
@@ -9,7 +9,7 @@ public class ReadingListComparisonProcessor {
 
     /** Reading lists. Note: The contained data are intentionally "repeated" from within the ReadingListComparisonDriver class. */
 
-    private Book[] bookReadingListPerson1;  // The book reading list for person 1. 
+    private Book[] bookReadingListPerson1;  // The book reading list for person 1. This might be able to be declared final (I'll have to study that).
 
     private Book[] bookReadingListPerson2;  // The book reading list for person 2.
 
@@ -18,19 +18,12 @@ public class ReadingListComparisonProcessor {
 
     private ArrayList<Book> booksInCommon = null;  // Books which are common to both people's reading lists.
 
-    // Authors which are common to both people's reading lists. In a simple format: "First name, Last name"
-    private ArrayList<String> authorsInCommon = null;
-
 
     /** Data structures for holding computed statistics. */
 
     private double percentageBooks1ComparedTo2 = 0.0;  // Percentage of books on person 1's list that are also on person 2's list.
 
-    private double percentageAuthors1ComparedTo2 = 0.0;  // Percentage of authors on person 1's list that are also on person 2's list.
-
     private double percentageBooks2ComparedTo1 = 0.0;  // Percentage of books on person 2's list that are also on person 1's list.
-
-    private double percentageAuthors2ComparedTo1 = 0.0;  // Percentage of authors on person 2's list that are also on person 1's list.
 
     // Data indicating the genre "preference" for person 1. Either FICTION or NON-FICTION
     // This will be computed by seeing if person 1 has >= 60% of 1 genre in their list.
@@ -40,21 +33,63 @@ public class ReadingListComparisonProcessor {
     // This will be computed by seeing if person 2 has >= 60% of 1 genre in their list.
     private boolean genrePreference2 = false;
 
+    // TODO: decide whether to have Book class access these values, since I'll need a way to associate a genre with each book.
     private static final boolean FICTION = true;  // Value signaling the fiction genre of book. TODO: pot. amend "GENRE" if it makes more sense in context
 
     private static final boolean NON_FICTION = false;  // Value signaling the non-fiction genre of book. TODO: pot. amend "GENRE" if it makes more sense in context
 
 
-    public ReadingListComparisonProcessor(ArrayList<Book> readingListPerson1, ArrayList<Book> readingListPerson2) {
-        this.bookReadingListPerson1 = bookReadingArrayListToArray(readingListPerson1);
-        this.bookReadingListPerson2 = bookReadingArrayListToArray(readingListPerson2);
+    public ReadingListComparisonProcessor(Book[] bookReadingListPerson1, Book[] bookReadingListPerson2) {
+        this.bookReadingListPerson1 = bookReadingListPerson1;
+        this.bookReadingListPerson2 = bookReadingListPerson2;
     }
 
-    private Book[] bookReadingArrayListToArray(ArrayList<Book> inputArrayList) {
-        Book[] result = new Book[inputArrayList.size()];
-        for (int i = 0; i < inputArrayList.size(); i++) {
-            result[i] = inputArrayList.get(i);
-        }
-        return result;
+
+    public Book[] getBookReadingListPerson1() {
+        return bookReadingListPerson1;
     }
+
+    // no setter needed because the book list won't be changed after initialization.
+
+    public Book[] getBookReadingListPerson2() {
+        return bookReadingListPerson2;
+    }
+
+    public ArrayList<Book> getBooksInCommon() {
+        return booksInCommon;
+    }
+
+    public void logComparisonBookReadingLists() {
+        // compare books (decompose by writing a method, and call it here)
+        logBooksInCommon();
+
+        // compare genres (decompose by writing a method, and call it here)
+    }
+
+    private void logBooksInCommon() {
+        if (smallerListPerson1()) {
+            // iterate through person 1's list
+            for (int i = 0; i < getBookReadingListPerson1().length; i++) {
+                if (getBookReadingListPerson1()[i].equals(getBookReadingListPerson2()[i])) {
+                    getBooksInCommon().add(getBookReadingListPerson1()[i]);
+                }
+            }
+        } else {
+            // iterate through person 2's list
+            for (int i = 0; i < getBookReadingListPerson2().length; i++) {
+                if (getBookReadingListPerson2()[i].equals(getBookReadingListPerson1()[i])) {
+                    getBooksInCommon().add(getBookReadingListPerson2()[i]);
+                }
+            }
+        }
+    }
+
+    private boolean smallerListPerson1() {
+        return getBookReadingListPerson1().length < getBookReadingListPerson2().length;
+    }
+
+    private void computeSharedInterestStatistics() {
+        // compute shared interest statistics, and store them in the appropriate fields.
+    }
+
 }
